7e1f0448223333f5b32811c9a68f8d56203a4a30
Dabeer Mirza <DabeerM@hotmail.com>
2023-04-02 18:07:29 +0100

    removed dead code

--- clicker.py
+++ clicker.py
@@ -1,12 +1,11 @@
 # import mouse
 import tkinter as tk
 
-def add_component(component):
-    component.pack()
 
 # variables
 window = tk.Tk(screenName="PyClicker")
 default_cadence = '1'
+default_pos_x, default_pos_y = '0', '0'
 btn_bg = '#e6edf0'
 btn_fg = '#03c2fc'
 
@@ -22,18 +21,22 @@ title = tk.Label(
     )
 add_component(title)
 
+frame_number_of_times = tk.Frame()
 num_of_times_label = tk.Label(
+    master=frame_number_of_times,
     text='How many times do you want to click on the same spot',
     foreground='blue',
     background='green',
     width=50,
     height=3
-    )
-num_of_times = tk.Entry(foreground='black',
-                         background='white',
-                         width=50)
-add_component(num_of_times_label)
-add_component(num_of_times)
+    ).pack()
+num_of_times = tk.Entry(
+    master=frame_number_of_times,
+    foreground='black',
+    background='white',
+    width=50
+    ).pack()
+frame_number_of_times.pack()
 
 #use pos_x.get() to get the value from the box
 frame_pos_x = tk.Frame()
@@ -44,15 +47,15 @@ pos_x_label = tk.Label(
     background='green',
     width=50,
     height=3
-    )
+    ).pack()
 pos_x = tk.Entry(
     master=frame_pos_x,
     foreground='black',
     background='white',
     width=50
     )
-add_component(pos_x_label)
-add_component(pos_x)
+pos_x.insert(index=tk.LEFT, string=default_pos_x)
+pos_x.pack()
 frame_pos_x.pack(side=tk.LEFT)
 
 frame_pos_y = tk.Frame()
@@ -63,15 +66,15 @@ pos_y_label = tk.Label(
     background='green',
     width=50,
     height=3
-    )
+    ).pack()
 pos_y = tk.Entry(
     master=frame_pos_y,
     foreground='black',
     background='white',
     width=50
     )
-add_component(pos_y_label)
-add_component(pos_y)
+pos_y.insert(index=tk.END, string=default_pos_y)
+pos_y.pack()
 frame_pos_y.pack(side=tk.LEFT)
 
 pick_click_location = tk.Button(
@@ -91,7 +94,7 @@ cadence_label = tk.Label(
     background='green',
     width=50,
     height=3
-    )
+    ).pack()
 cadence = tk.Entry(
     master=frame_cadence,
     foreground='black',
@@ -99,8 +102,7 @@ cadence = tk.Entry(
     width=50
     )
 cadence.insert(index=tk.END, string=default_cadence)
-add_component(cadence_label)
-add_component(cadence)
+cadence.pack()
 frame_cadence.pack(side=tk.LEFT)
 
 start_btn = tk.Button(
