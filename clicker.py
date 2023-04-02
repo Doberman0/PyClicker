fa88322ac86c319ac34e3655250af5bbb70f6eb7
Dabeer Mirza <DabeerM@hotmail.com>
2023-04-02 19:07:35 +0100

    rename variables to follow tkinter naming conventions

--- clicker.py
+++ clicker.py
@@ -6,112 +6,114 @@ import tkinter as tk
 window = tk.Tk(screenName="PyClicker")
 default_cadence = '1'
 default_pos_x, default_pos_y = '0', '0'
+default_number_of_clicks = '1'
 btn_bg = '#e6edf0'
 btn_fg = '#03c2fc'
 
 # mouse.move(x, y, absolute=True, duration=0.1)
 # mouse.click('left')
 
-title = tk.Label(
+lbl_title = tk.Label(
     text='PyClicker',
     foreground='blue',
     background='green',
     width=10,
     height=3
-    )
-add_component(title)
+    ).pack()
 
-frame_number_of_times = tk.Frame()
-num_of_times_label = tk.Label(
-    master=frame_number_of_times,
+frm_number_of_times = tk.Frame()
+lbl_num_of_times = tk.Label(
+    master=frm_number_of_times,
     text='How many times do you want to click on the same spot',
     foreground='blue',
     background='green',
     width=50,
     height=3
     ).pack()
-num_of_times = tk.Entry(
-    master=frame_number_of_times,
+ent_num_of_times = tk.Entry(
+    master=frm_number_of_times,
     foreground='black',
     background='white',
     width=50
-    ).pack()
-frame_number_of_times.pack()
+    )
+ent_num_of_times.insert(tk.END, default_number_of_clicks)
+ent_num_of_times.pack()
+frm_number_of_times.pack()
 
 #use pos_x.get() to get the value from the box
-frame_pos_x = tk.Frame()
+frm_pos_x = tk.Frame()
 pos_x_label = tk.Label(
-    master=frame_pos_x,
+    master=frm_pos_x,
     text='Position X:',
     foreground='blue',
     background='green',
     width=50,
     height=3
     ).pack()
-pos_x = tk.Entry(
-    master=frame_pos_x,
+ent_pos_x = tk.Entry(
+    master=frm_pos_x,
     foreground='black',
     background='white',
     width=50
     )
-pos_x.insert(index=tk.LEFT, string=default_pos_x)
-pos_x.pack()
-frame_pos_x.pack(side=tk.LEFT)
+ent_pos_x.insert(tk.END, default_pos_x)
+ent_pos_x.pack()
+frm_pos_x.pack(side=tk.LEFT)
 
-frame_pos_y = tk.Frame()
-pos_y_label = tk.Label(
-    master=frame_pos_y,
+frm_pos_y = tk.Frame()
+lbl_pos_y = tk.Label(
+    master=frm_pos_y,
     text='Position Y:',
     foreground='blue',
     background='green',
     width=50,
     height=3
     ).pack()
-pos_y = tk.Entry(
-    master=frame_pos_y,
+ent_pos_y = tk.Entry(
+    master=frm_pos_y,
     foreground='black',
     background='white',
     width=50
     )
-pos_y.insert(index=tk.END, string=default_pos_y)
-pos_y.pack()
-frame_pos_y.pack(side=tk.LEFT)
+ent_pos_y.insert(tk.END, default_pos_y)
+ent_pos_y.pack()
+frm_pos_y.pack(side=tk.LEFT)
 
-pick_click_location = tk.Button(
+btn_pick_click_location = tk.Button(
     text='Click anywhere',
     width=25,
     height=5,
     background=btn_bg,
     foreground=btn_fg
 )
-pick_click_location.pack()
+btn_pick_click_location.pack(side=tk.LEFT)
 
-frame_cadence = tk.Frame()
-cadence_label = tk.Label(
-    master=frame_cadence,
+frm_cadence = tk.Frame()
+lbl_cadence = tk.Label(
+    master=frm_cadence,
     text='Cadence (seconds):',
     foreground='blue',
     background='green',
     width=50,
     height=3
     ).pack()
-cadence = tk.Entry(
-    master=frame_cadence,
+ent_cadence = tk.Entry(
+    master=frm_cadence,
     foreground='black',
     background='white',
     width=50
     )
-cadence.insert(index=tk.END, string=default_cadence)
-cadence.pack()
-frame_cadence.pack(side=tk.LEFT)
+ent_cadence.insert(index=tk.END, string=default_cadence)
+ent_cadence.pack()
+frm_cadence.pack(side=tk.LEFT)
 
-start_btn = tk.Button(
+btn_start = tk.Button(
     text='Start',
     width=25,
     height=5,
     background=btn_bg,
     foreground=btn_fg
 )
-start_btn.pack()
+btn_start.pack()
 
 window.mainloop()
